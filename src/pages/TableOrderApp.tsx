39164eaca9d0477866d1c08ec7697a68aa22ff08
[dyad] <git@dyad.sh>
2026-03-29 22:18:09 -0300

    [dyad] Implementação completa do ecossistema de salão: App do Garçom, Autoatendimento via QR Code e Gestão de Mesas. - wrote 4 file(s)

--- src/pages/TableOrderApp.tsx
+++ src/pages/TableOrderApp.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { 
   Utensils, Receipt, Plus, Minus, ShoppingBag, 
